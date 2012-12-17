5d27d383ab3fdae87ab3836ebb1d65e2f9d69941
matt venn <pi@raspberrypi.(none)>
2012-12-17 17:58:15 +0000

    first go at twitter -> vaccuum display

---
+++ tweettube/tweettube.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import os
+import tweepy, sys, pdb, time
+from authfile import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+print "polling mentions for account " + api.me().name
+lastid = api.mentions(count=1)[0].id
+while True:
+	mentions = api.mentions(since_id=lastid)
+	for i in mentions:
+		print "incoming mention: " + i.text
+		os.system("sudo ./tube_driver.py --text '%s'" % i.text)
+		lastid = i.id
+	time.sleep(30)
